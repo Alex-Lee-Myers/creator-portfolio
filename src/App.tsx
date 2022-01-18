76550f108745f11b89fe2988d4cbade17fe2335d
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-01-17 20:45:22 -0500

    Nav bar template put in with styling. Ready for React-Router-Dom setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
 import GlobalStyle from './styles/GlobalStyle';
+import Nav from './components/Nav';
 
 const App: React.FunctionComponent = () => {
 
@@ -10,6 +11,7 @@ const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs />
     </div>
   );
